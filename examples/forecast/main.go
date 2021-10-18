ddec235eb234f4be80833f7bbd2cc1ac1298358d
The ZZ <ztimes2@pm.me>
2021-10-18 05:25:18 +0000

    WIP

--- examples/forecast/main.go
+++ examples/forecast/main.go
@@ -1,41 +1,65 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/ztimes2/surfforecast-go"
+	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	ff, err := surfforecast.New().ForecastsForEightDays("cherating")
-	if err != nil {
+	/*
+		ff, err := surfforecast.New().ForecastsForEightDays("cherating")
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Issued at:", ff.IssuedAt)
+
+		f := ff.Daily[0]
+
+		fmt.Printf("Date: %s\n", f.Timestamp)
+
+		for _, hf := range f.Hourly {
+			fmt.Println()
+			fmt.Println("----------------------------------------")
+
+			fmt.Printf("Date: %s\n", hf.Timestamp)
+			fmt.Printf("Rating (0-10): %d\n", hf.Rating)
+			fmt.Printf("Wave energy (kJ): %v\n", hf.WaveEnergyInKiloJoules)
+			fmt.Printf("Wind speed (km/h): %v\n", hf.Wind.SpeedInKilometersPerHour)
+			fmt.Printf("Wind direction to (degrees): %v\n", hf.Wind.DirectionToInDegrees)
+			fmt.Printf("Wind direction from (compass points): %s\n", hf.Wind.DirectionFromInCompassPoints)
+			fmt.Printf("Wind state: %s\n", hf.Wind.State)
+
+			for i, swell := range hf.Swells {
+				fmt.Printf("Swell #%d:\n", i+1)
+				fmt.Printf("\tPeriod (s): %v\n", swell.PeriodInSeconds)
+				fmt.Printf("\tDirection to (degrees): %v\n", swell.DirectionToInDegrees)
+				fmt.Printf("\tDirection from (compass points): %s\n", swell.DirectionFromInCompassPoints)
+				fmt.Printf("\tWave height (m): %v\n", swell.WaveHeightInMeters)
+			}
+		}
+	*/
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	var page string
+	if err := chromedp.Run(ctx,
+		chromedp.Navigate("https://www.surf-forecast.com/breaks/cherating/forecasts/latest"),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			n, err := dom.GetDocument().Do(ctx)
+			if err != nil {
+				return err
+			}
+			page, err = dom.GetOuterHTML().WithNodeID(n.NodeID).Do(ctx)
+			return err
+		}),
+	); err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Issued at:", ff.IssuedAt)
-
-	f := ff.Daily[0]
-
-	fmt.Printf("Date: %s\n", f.Timestamp)
-
-	for _, hf := range f.Hourly {
-		fmt.Println()
-		fmt.Println("----------------------------------------")
-
-		fmt.Printf("Date: %s\n", hf.Timestamp)
-		fmt.Printf("Rating (0-10): %d\n", hf.Rating)
-		fmt.Printf("Wave energy (kJ): %v\n", hf.WaveEnergyInKiloJoules)
-		fmt.Printf("Wind speed (km/h): %v\n", hf.Wind.SpeedInKilometersPerHour)
-		fmt.Printf("Wind direction to (degrees): %v\n", hf.Wind.DirectionToInDegrees)
-		fmt.Printf("Wind direction from (compass points): %s\n", hf.Wind.DirectionFromInCompassPoints)
-		fmt.Printf("Wind state: %s\n", hf.Wind.State)
-
-		for i, swell := range hf.Swells {
-			fmt.Printf("Swell #%d:\n", i+1)
-			fmt.Printf("\tPeriod (s): %v\n", swell.PeriodInSeconds)
-			fmt.Printf("\tDirection to (degrees): %v\n", swell.DirectionToInDegrees)
-			fmt.Printf("\tDirection from (compass points): %s\n", swell.DirectionFromInCompassPoints)
-			fmt.Printf("\tWave height (m): %v\n", swell.WaveHeightInMeters)
-		}
-	}
+	fmt.Println(page)
 }
--- go.mod
+++ go.mod
@@ -5,3 +5,15 @@ go 1.17
 require golang.org/x/net v0.0.0-20211014222326-fd004c51d1d6
 
 require github.com/tkuchiki/go-timezone v0.2.2
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20211015214701-1037196e2fdd // indirect
+	github.com/chromedp/chromedp v0.7.4 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/sys v0.0.0-20211015200801-69063c4bb744 // indirect
+)
